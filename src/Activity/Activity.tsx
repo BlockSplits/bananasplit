f56aabb275d0b4aeab54f9d5daa6db3184310365
Wixzi <wixzi@pm.me>
2022-10-16 19:18:04 -0500

    Added folders and Typescript

---
+++ src/Activity/Activity.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Activity() {
+  return (
+    <View style={styles.container}>
+      <Text>Activity Page</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
